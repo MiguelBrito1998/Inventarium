ebfbc8c0cf188a4455e29cc31f7658d75d11b06c
MiguelBrito1998 <miguelbrito869@gmail.com>
2023-03-28 00:07:44 -0400

    Api
    Se intento crear el dato json de ventas y se logro crear la api para dirigir a la ruta del html

---
+++ ProyectoFinal/inter.py
@@ -0,0 +1,14 @@
+
+from flask import Flask, send_file
+# from flask import request
+
+app=Flask(__name__)
+
+@app.route('/')
+def Prueba():
+    print("Aqui estoy")
+    return send_file('./Template/indice.html')
+app.run(debug=True)
+
+
+
